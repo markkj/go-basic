23bbf57a2225227993c501836ed1881132dda74e
markkj <electormark@gmail.com>
2021-08-20 15:56:27 +0700

    update

--- cards/main.go
+++ cards/main.go
@@ -1,9 +1,14 @@
 package main
 
-import "fmt"
-
 func main() {
-	var card string = "Ace of Spades"
-	fmt.Println(card)
+	// cards := deck{newCard(), newCard(), newCard()}
+	// cards = append(cards, "Six of Spades")
+	// cards.print()
+	cards := newDeck()
+	cards.print()
 
+}
+
+func newCard() string {
+	return "Five of Diamonds"
 }
