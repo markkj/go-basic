9c1dc695b4072ea1979294b2ae613201b8b75d99
markkj <electormark@gmail.com>
2021-09-04 22:31:47 +0700

    add goroutine2

---
+++ fundamental/goroutine2/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+// this tutorial in fucntion doSomethingA it maybe run or not run
+func main() {
+	c := make(chan string)
+	go doSomethingA()
+	go doSomethingB(c)
+	go doSomethingC(c)
+	fmt.Println(<-c)
+	fmt.Println(<-c)
+
+}
+
+func doSomethingA() {
+	time.Sleep(randomeTime() * time.Second)
+	fmt.Println("Do Something in A")
+
+}
+
+func doSomethingB(message chan<- string) {
+	time.Sleep(randomeTime() * time.Second)
+	message <- "Do Something in B"
+
+}
+
+func doSomethingC(message chan<- string) {
+	time.Sleep(randomeTime() * time.Second)
+	message <- "Do Something in C"
+
+}
+
+func randomeTime() time.Duration {
+	rand.Seed(time.Now().UnixNano())
+	n := rand.Intn(3)
+	return time.Duration(n)
+}
