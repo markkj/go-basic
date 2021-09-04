5b98f09b7480c3fcf09e5f80147a53293c9540e1
markkj <electormark@gmail.com>
2021-09-04 22:51:07 +0700

    update

---
+++ fundamental/struct/main.go
@@ -0,0 +1,43 @@
+package main
+
+import "fmt"
+
+type person struct {
+	firstname string
+	lastname  string
+}
+
+type author struct {
+	startDate string
+	person
+}
+
+type blogPost struct {
+	title string
+	author
+}
+
+func main() {
+	peter := person{
+		firstname: "Peter",
+		lastname:  "Jologo",
+	}
+	author1 := author{
+		person: peter,
+	}
+	firstPost := blogPost{
+		title:  "firstPost",
+		author: author1,
+	}
+	firstPost.printDetails()
+}
+
+func (b blogPost) printDetails() {
+	fmt.Println("title :", b.title)
+	b.author.printDetails()
+}
+
+func (p *person) printDetails() {
+	fmt.Println("fistname :", p.firstname)
+	fmt.Println("lastname :", p.lastname)
+}
