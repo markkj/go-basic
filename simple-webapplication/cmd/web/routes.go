74f97ee23b46b7ba6c4221d9eac85a40fc01097a
markkj <electormark@gmail.com>
2021-09-05 17:34:54 +0700

    use router with chi and use middleware

--- simple-webapplication/cmd/web/routes.go
+++ simple-webapplication/cmd/web/routes.go
@@ -3,13 +3,18 @@ package main
 import (
 	"net/http"
 
-	"github.com/bmizerany/pat"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+
 	"github.com/markkj/simple-webapplication/pkg/config"
 	"github.com/markkj/simple-webapplication/pkg/handlers"
 )
 
 func routes(app *config.AppConfig) http.Handler {
-	mux := pat.New()
+	mux := chi.NewRouter()
+
+	mux.Use(middleware.Recoverer)
+
 	mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
 	mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
 
