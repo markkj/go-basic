dc6be1be606746dd4921df8773f361a7dbcf126f
markkj <electormark@gmail.com>
2021-09-05 14:50:57 +0700

    first commit

---
+++ simple-webapplication/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
+		rw.Write([]byte("hello"))
+	})
+
+	http.ListenAndServe(":8080", nil)
+}
