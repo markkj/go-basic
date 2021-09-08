363128035e48470b48e293f244ec3dad38b5531b
markkj <electormark@gmail.com>
2021-09-08 14:05:03 +0700

    add cacahe

--- design-pattern/adapter/example1/main.go
+++ design-pattern/adapter/example1/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"crypto/md5"
+	"encoding/json"
 	"fmt"
 	"strings"
 )
@@ -71,6 +73,8 @@ type vectorToRasterAdapter struct {
 	points []Point
 }
 
+var pointCache = map[[16]byte][]Point{}
+
 func (v *vectorToRasterAdapter) GetPoint() []Point {
 	return v.points
 }
@@ -95,11 +99,44 @@ func (v *vectorToRasterAdapter) addLine(line Line) {
 
 }
 
+func (v *vectorToRasterAdapter) addLineCache(line Line) {
+	hash := func(obj interface{}) [16]byte {
+		bytes, _ := json.Marshal(obj)
+		return md5.Sum(bytes)
+	}
+	h := hash(line)
+	if pts, ok := pointCache[h]; ok {
+		for _, pt := range pts {
+			v.points = append(v.points, pt)
+		}
+		return
+	}
+
+	left, right := minmax(line.X1, line.X2)
+	top, bottom := minmax(line.Y1, line.Y2)
+	dx := right - left
+	dy := line.Y2 - line.Y1
+
+	if dx == 0 {
+		for y := top; y <= bottom; y++ {
+			v.points = append(v.points, Point{left, y})
+		}
+	} else if dy == 0 {
+		for x := left; x <= right; x++ {
+			v.points = append(v.points, Point{x, top})
+		}
+	}
+
+	pointCache[h] = v.points
+	fmt.Println("generated", len(v.points), "points")
+
+}
+
 func VectorToRaster(v *VectorImage) RasterImage {
 	adapter := &vectorToRasterAdapter{}
 
 	for _, line := range v.Lines {
-		adapter.addLine(line)
+		adapter.addLineCache(line)
 	}
 
 	return adapter
